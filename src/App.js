0aa0f42a41c44726f3d0bd03805858acd14647fd
TiaguMB <tiagomb.privado@gmail.com>
2024-09-07 20:46:44 -0300

    feat: added post component with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function App() {
-	return <h1>Componente App</h1>;
-}
+import Post from "./Post";
 
-export default App;
+export default function App() {
+	return (
+		<>
+			<h1>JStack's Blog</h1>
+			<h2>Posts da semana</h2>
+
+			<hr />
+
+			<Post
+				title="Título da notícia 01"
+				subtitle="Subtítulo da notícia 01"
+			/>
+			<Post
+				title="Título da notícia 02"
+				subtitle="Subtítulo da notícia 02"
+			/>
+			<Post
+				title="Título da notícia 03"
+				subtitle="Subtítulo da notícia 03"
+			/>
+		</>
+	);
+}

3297f2357e8e9e674ababe1b86c57b02a3f6bf71
TiaguMB <tiagomb.privado@gmail.com>
2024-09-09 21:36:11 -0300

    feat: add PropTypes and defaultProps

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@ import Header from "./Header";
 export default function App() {
 	return (
 		<>
-			<Header title="JStack's Blog">
+			<Header>
 				<h2>Posts da semana</h2>
 			</Header>
 
 			<hr />
 
 			<Post
-				title="Título da notícia 01"
-				subtitle="Subtítulo da notícia 01"
+				likes={20}
+				post={{
+					title: "Título da notícia 01",
+					subtitle: "Subtítulo da notícia 01"
+				}}
 			/>
 			<Post
-				title="Título da notícia 02"
-				subtitle="Subtítulo da notícia 02"
+				likes={10}
+				post={{
+					title: "Título da notícia 02",
+					subtitle: "Subtítulo da notícia 02"
+				}}
 			/>
 			<Post
-				title="Título da notícia 03"
-				subtitle="Subtítulo da notícia 03"
+				likes={50}
+				post={{
+					title: "Título da notícia 03",
+					subtitle: "Subtítulo da notícia 03"
+				}}
 			/>
 		</>
 	);
--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Post(props) {
 	return (
 		<>
 			<article>
-				<strong>{props.title}</strong><br />
-				<small>{props.subtitle}</small>
+				<strong>{props.post.title}</strong><br />
+				<small>{props.post.subtitle}</small>
+				<br />
+				Media: {props.likes / 2}
 			</article>
 			<br />
 		</>
 	);
+}
+
+Post.PropTypes = {
+	likes: PropTypes.number.isRequired,
+	post: PropTypes.shape({
+		title: PropTypes.string.isRequired,
+		subtitle: PropTypes.string.isRequired,
+	}).isRequired,
 }
 
 export default Post;

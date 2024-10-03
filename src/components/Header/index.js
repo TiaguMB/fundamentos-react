b08561d70b6997b2f9f8ff3f24a438621d5875e3
TiaguMB <tiagomb.privado@gmail.com>
2024-10-02 23:34:51 -0300

    feat: goto home function onClick button (useHistory)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 export default function Header() {
 	const { selectedTheme, onToggleTheme } = useContext(ThemeContext);
+
+	const history = useHistory();
+
+	function handleNavigate() {
+		history.push('/');
+	}
 
 	return (
 		<Container>
@@ -14,6 +21,12 @@ export default function Header() {
 				onClick={onToggleTheme}
 			>
 				{selectedTheme === 'dark' ? '🌞' : '🌚'}
+			</button>
+			<button
+				onClick={handleNavigate}
+				style={{ color: '#fff' }}
+			>
+				Navegar
 			</button>
 		</Container>
 	);
